f20745ec75fa1585eb0b6fa51abf0541dbf3df42
smolfly <68849295+smolfly@users.noreply.github.com>
2021-02-12 22:01:52 +0300

    1st option

---
+++ src/color-switch.js
@@ -0,0 +1,45 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+  const refs = {
+    startButton: document.querySelector('[data-action="start"]'),
+    stopButton: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body'),
+  };
+
+
+  let interval = null;
+
+  
+  
+  function startChanging() {
+    if (!interval) {
+      interval = setInterval(() => {
+        randomIntegerFromInterval(0, [colors.length - 1]);
+        refs.body.setAttribute(
+          'style',
+          `background: ${
+            colors[randomIntegerFromInterval(0, [colors.length - 1])]
+          }`,
+        );
+      }, 1000);
+    }
+  }
+  
+  function stopChanging() {
+    clearInterval(interval);
+    interval = null;
+  }
+
+  refs.startButton.addEventListener('click', startChanging);
+  refs.stopButton.addEventListener('click', stopChanging);
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
