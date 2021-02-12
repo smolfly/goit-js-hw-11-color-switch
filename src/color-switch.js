83c88d4f3452a0bedda3418c8986528363f04da3
smolfly <68849295+smolfly@users.noreply.github.com>
2021-02-12 22:49:03 +0300

    Update color-switch.js

--- src/color-switch.js
+++ src/color-switch.js
@@ -7,20 +7,25 @@ const colors = [
     '#795548',
   ];
 
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
   const refs = {
     startBtn: document.querySelector('[data-action="start"]'),
     stopBtn: document.querySelector('[data-action="stop"]'),
     body: document.querySelector('body'),
   };
 
-
-  let intervalId= null;
-
-  
-  
-  function startChanging() {
-    if (!intervalId) {
-      intervalId = setInterval(() => {
+  const timer = {
+    intervalId: null,
+    isActive: false,
+    start() {
+      if (this.isActive) {
+        return;
+      }
+      this.isActive = true;
+      this.intervalId = setInterval(() => {
         randomIntegerFromInterval(0, [colors.length - 1]);
         refs.body.setAttribute(
           'style',
@@ -29,17 +34,46 @@ const colors = [
           }`,
         );
       }, 1000);
-    }
-  }
+    },
   
-  function stopChanging() {
-    clearInterval(intervalId);
-    intervalId = null;
-  }
-
-  refs.startBtn.addEventListener('click', startChanging);
-  refs.stopBtn.addEventListener('click', stopChanging);
-
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    stop() {
+      this.isActive = false;
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    },
   };
+  
+  refs.startBtn.addEventListener('click', timer.start.bind(timer));
+  refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+
+
+  
+//  Вариант с функцией:
+
+//   let intervalId= null;
+  
+//   function startChanging() {
+//     if (!intervalId) {
+//       intervalId = setInterval(() => {
+//         randomIntegerFromInterval(0, [colors.length - 1]);
+//         refs.body.setAttribute(
+//           'style',
+//           `background: ${
+//             colors[randomIntegerFromInterval(0, [colors.length - 1])]
+//           }`,
+//         );
+//       }, 1000);
+//     }
+//   }
+  
+//   function stopChanging() {
+//     clearInterval(intervalId);
+//     intervalId = null;
+//   }
+
+//   refs.startBtn.addEventListener('click', startChanging);
+//   refs.stopBtn.addEventListener('click', stopChanging);
+
+//   const randomIntegerFromInterval = (min, max) => {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   };
